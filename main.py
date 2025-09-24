35a830d1d64b71c9874cd9c32b2421631215e3d0
Masayume88 <fmaas111@outlook.com>
2025-09-24 14:56:55 +0200

    3rd commit. Added fn word_count() to split file str in words, def sep, count words, output total amount of words via main() call

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
-    print(text)
+    # Check ok. File contents from filepath parsed as string, output string.
+    # print(text)
+    num_words = word_count(text)
+    print(f"Found {num_words} total words")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def word_count(text):
-    ...
+    word_list = text.split()
+    word_count = 0
+
+    for word in word_list:
+        #Check ok. Are the words stored, sep whitespace, correctly.
+        # print(word)
+        word_count += 1
+    
+    return word_count
 
 main()
