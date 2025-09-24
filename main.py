9c526f75b1d3bb2895c421c5eb20a362e7e045bd
Masayume88 <fmaas111@outlook.com>
2025-09-24 14:37:17 +0200

    2nd commit. Added fn to read file of path as str and output via main()

---
+++ main.py
@@ -0,0 +1,14 @@
+#tbd
+def main():
+    path_to_file = "books/frankenstein.txt"
+    text = get_book_text(path_to_file)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(text):
+    ...
+
+main()
