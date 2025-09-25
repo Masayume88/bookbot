c88fb4845514a3abaf1b49edc6e87f555bc1d4a0
Masayume88 <fmaas111@outlook.com>
2025-09-25 14:42:37 +0200

    5th commit. Added sorted list/dict functionality to cleanly print the count per alphabetical character

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from stats import word_count
+from stats import word_count, character_count, sorted_character_count
 
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
-    # Check ok. File contents from filepath parsed as string, output string.
-    # print(text)
     num_words = word_count(text)
+    num_characters = character_count(text)
+    sorted_num_characters = sorted_character_count(num_characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_num_characters:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -8,3 +8,31 @@ def word_count(text):
         word_count += 1
     
     return word_count
+
+def character_count(text):
+    character_string = text.lower()
+    character_dict = {}
+
+    for character in character_string:
+        if character not in character_dict:
+            character_dict[character] = 1
+        else:
+            character_dict[character] += 1
+    
+    return character_dict
+
+def sort_on(item):
+    return item["num"]
+
+def sorted_character_count(character_dict):
+    sorted_character_list = []
+
+    for ch, count in character_dict.items():
+        if ch.isalpha():
+            entry = {"char": ch, "num": count}
+            sorted_character_list.append(entry)
+    
+    return sorted(sorted_character_list, reverse=True, key=sort_on)
+
+
+
